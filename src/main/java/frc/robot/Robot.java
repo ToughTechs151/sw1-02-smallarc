1f8e05aa92836e1385f02194372de45830f7751c
Brian Utterback <brian@utterback.org>
2020-12-09 16:47:29 -0500

    Add description of our robot base.
    Add a description of the differences between TimeRobot and IterativeRobot.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,8 +4,33 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.RobotContainer;
 
+/**
+ * We create the robot using the TimedRobot base. The difference between
+ * the IimedRobot base and the other common choice, IterativeRobot, is that
+ * iterative calls the periodic methods each time there is a new packet
+ * from the drive station, while the TimedRobot calls them each 20ms.
+ * Since the drive station typically sends its data each 20ms there isn't 
+ * much difference, but there can be jitter in the drive station and in the
+ * network so the actual time at which they run can be off. When we use
+ * control loops like the PID loop pattern, this can have an effect on 
+ * the calculations. So TimedRobot is a bit more stable. 
+ **/
 public class Robot extends TimedRobot {
-
+  /**
+   * We move robot configuration information out of the Robot.java
+   * file into a separate RobotContainer file. This separates things
+   * that can change as the hardware changes, i.e. physical parts, 
+   * from the actions that those things need to take, i.e. the code
+   * that provides the logic behind them. Since this example uses
+   * the real Command Based Robot pattern, we don't actually have to 
+   * do much other than create the Robot in the container and run the
+   * scheduler. In this small case we still will not move the
+   * subsystem and commands into their own files. Instead we will 
+   * leave them in the robot container. 
+   **/
+  /* 
+   * We override the robotInit method here to create the robot in the
+   * robot container. The 
   @Override
   public void robotInit() {
     new RobotContainer();

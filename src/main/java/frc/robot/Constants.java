863d2b721260d36bf12682bad1db827542774821
Brian Utterback <brian@utterback.org>
2020-12-09 16:30:07 -0500

    Add comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,12 @@
 package frc.robot;
-
+/**
+ * We create constants all in one file so we can easily find and change them.
+ * By convention we start each constant with a lower case "k" to make them
+ * obvious in elsewhere. We can group constants together when they are 
+ * functionally related. In this case we have a class for all the constants
+ * related to the drive train and a class for all the constants related to 
+ * the Operator Interface (OI). 
+ **/
 public final class Constants {
     public static final class DriveConstants {
         public static final int kLeftMotorPort = 0;
